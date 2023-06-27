adc96b54ddd77d14a48f316bf2d7c86b04dcd0d6
phelsdon <phelsdon@previon.ch>
2023-06-27 19:42:09 +0200

    add super user, controllers, routes and views for index and reports

--- feed/feedapp/admin.py
+++ feed/feedapp/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from .models import User, Post, Report
 
 # Register your models here.
+admin.site.register(User, UserAdmin)
+admin.site.register(Post) # first created in models then makemigrations and migrate;: and registered here
+admin.site.register(Report) # first created in models then makemigrations and migrate;: and registered here - move on to views
+
+
--- feed/feedapp/views.py
+++ feed/feedapp/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
 
 # Create your views here.
+def index(request):
+    return render(request, 'feedapp/index.html')
+
+def reports(request):
+    return render(request, 'feedapp/reports.html')
